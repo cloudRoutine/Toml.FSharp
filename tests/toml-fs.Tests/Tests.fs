ab8332f392db54b2f961e6fa91df509e26a68c50
Jared Hester <jared.hester@gmail.com>
2015-11-20 04:13:56 -0500

    update project configs

--- tests/toml-fs.Tests/Tests.fs
+++ tests/toml-fs.Tests/Tests.fs
@@ -1,10 +1,4 @@
-module toml-fs.Tests
+module TomlFs.Tests
 
-open toml-fs
-open NUnit.Framework
 
-[<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+
