390cff0542d7551ff0d54ad3ed6836e6526d38b1
Jared Hester <jared.hester@gmail.com>
2015-11-20 21:31:57 -0500

    reorganized test script

--- src/toml-fs/Script.fsx
+++ src/toml-fs/Script.fsx
@@ -9,60 +9,18 @@ open FParsec
 open TomlFs.AST
 open TomlFs.Parsers
 
-type UserState  = unit 
-type Parser<'t> = Parser<'t,UserState>
-
-open System.Runtime.CompilerServices
-
-
-// TODO - Invesigate whether 
-//  [<MethodImpl (MethodImplOptions.AggressiveInlining)>]
-// makes a difference
-
-// TODO - Proper checks for `_` rules
-
-
-let inline notChar (c1:char) (c2:char) = uint32 c1 <> uint32 c2
-//let pBareKey: Parser<_> = many1CharsTill (digit<|>letter<|>``_``<|>``-``) unicodeSpaces1
-//
-////let pInt: Parser<_> = 
-////    let firstDigit: Parser<_> = satisfy (notChar '0'|&|isDigit)
-//    let signed: Parser<_>  = anyOf['+';'-'] >>. firstDigit
-//    let intMid: Parser<_>  =  
-
-
-
 let date1 = "1979-05-27T07:32:00Z"
 let date2 = "1979-05-27T00:32:00-07:00"
 let date3 = "1979-05-27T00:32:00.999999-07:00"
 let date4 = "1979-05-27T07:32:00"
 let date5 = "1979-05-27T00:32:00.999999"
 let date6 = "1979-05-27"
-;;
 
-[
-    date1
-    date2
-    date3
-    date4
-    date5
-    date6   ] 
-|> List.iter (fun x -> run pDateTime_toml x  |> printfn "%A");;
-                          
 let int0 = "1_000"  
 let int1 = "5_349_221" 
 let int2 = "1_2_3_4_5" 
 let int3 = "0_2_3_4_5" 
 let int4 = "72345" 
-;;
-
-[   int0
-    int1
-    int2
-    int3
-    int4    ]    
-|> List.iter (fun x -> run pInt64_toml x |> printfn "%A" );;
-
 
 let flt0 = "9._224_617  .445_991_228_313"
 let flt1 = "+1.0"
@@ -72,52 +30,18 @@ let flt4 = "5e+22"
 let flt5 = "1e6"
 let flt6 = "-2E-2"
 let flt7 = "6.626e-34"
-;;
-
-[   flt0
-    flt1
-    flt2
-    flt3
-    flt4
-    flt5
-    flt6
-    flt7    ]
-|> List.iter (fun x -> run pFloat_toml x |> printfn "%A" );;
-
-run toml_inlineTable 
-    "{ one = 1, two = 2, three = 3}";;
-
-run pString_toml "\"hello\"";;
-
-run toml_array  "[ 22.04 , 234.00, 23_4.304]";;
-
-run toml_array "[ { x = 1, y = 2, z = 3 }, { x = 7, y = 8, z = 9 }, { x = 2, y = 4, z = 8 } ]";;
-
-//
-//run parseArray 
-//    "[   \"hello\", \"watup\", \"yo\" ] ";;
-//
-//run parseArray """[ [1,2,3], [1.0,2.0,3.0], ["a","b","c"] ]""";;
 
 let table0 = """[owner]
 name = "Tom Preston-Werner"
 dob = 1979-05-27T07:32:00-08:00 # First class dates
 """
-(* 
-
-server = "192.168.1.1"   
-
-
-*)
 
 let table1="""[database]
 connection_max = 5000
+server = "192.168.1.1"   
 ports = [ 8001, 8001, 8002 ]
 enabled = true
 """
-
-run pTable table0;;
-run pTable table1;;
 
 let toml0 = """
 
@@ -273,8 +197,40 @@ test_string = "You'll hate me after this - #"          # " Annoying, isn't it?
 
 """
 ;;
+[   int0
+    int1
+    int2
+    int3
+    int4    ]    
+|> List.iter (fun x -> run pInt64_toml x |> printfn "%A" );;
 
+[   flt0
+    flt1
+    flt2
+    flt3
+    flt4
+    flt5
+    flt6
+    flt7    ]
+|> List.iter (fun x -> run pFloat_toml x |> printfn "%A" );;
 
+[
+    date1
+    date2
+    date3
+    date4
+    date5
+    date6   ] 
+|> List.iter (fun x -> run pDateTime_toml x  |> printfn "%A");;
+
+run toml_inlineTable  "{ one = 1, two = 2, three = 3}";;
+run pString_toml "\"hello\"";;
+run toml_array  "[   \"hello\", \"watup\", \"yo\" ] ";;
+run toml_array """[ [1,2,3], [1.0,2.0,3.0], ["a","b","c"] ]""";;
+run toml_array  "[ 22.04 , 234.00, 23_4.304]";;
+run toml_array "[ { x = 1, y = 2, z = 3 }, { x = 7, y = 8, z = 9 }, { x = 2, y = 4, z = 8 } ]";;
+run pTable table0;;
+run pTable table1;;
 
 
 printfn "toml 0"
